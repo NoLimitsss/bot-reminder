e663b59be9161d524373365e4e659b7baad9622f
ProudLion <63865632+NoLimitsss@users.noreply.github.com>
2026-06-17 17:35:00 +0300

    v2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const tg = window.Telegram.WebApp;
 tg.ready();
 
-// Подставляем имя пользователя
+// Имя пользователя
 if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
     document.getElementById('username').innerText = tg.initDataUnsafe.user.first_name;
+}
+
+// Функция переключения экранов
+function showScreen(screenName) {
+    document.getElementById('main-screen').style.display = (screenName === 'main') ? 'block' : 'none';
+    document.getElementById('add-screen').style.display = (screenName === 'add') ? 'flex' : 'none';
 }
