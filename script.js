9238fab8bb591c988344093ffc50bc27b097ce4f
ProudLion <63865632+NoLimitsss@users.noreply.github.com>
2026-06-17 20:13:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
         const splash = document.getElementById('splash-screen');
         splash.style.opacity = '0';
         splash.style.visibility = 'hidden';
-    }, 2000);
+    }, 1500);
 });
 // Обработка выбора даты и времени
 document.getElementById('real-date').addEventListener('change', function() {
