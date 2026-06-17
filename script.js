2d469c8e065580adc11b76c2d4206f917c9426f0
ProudLion <63865632+NoLimitsss@users.noreply.github.com>
2026-06-17 17:28:20 +0300

    v1

---
+++ script.js
@@ -0,0 +1,7 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
+
+// Подставляем имя пользователя
+if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+    document.getElementById('username').innerText = tg.initDataUnsafe.user.first_name;
+}
